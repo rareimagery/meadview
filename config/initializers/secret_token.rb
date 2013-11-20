6d8f42b87aec72e1bb3891f94faf8a131f9e0058
rareimagery <rareblue@gmail.com>
2013-11-19 17:30:15 -0700

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meadview::Application.config.secret_key_base = 'e5bc876ba5f509de578fb3c421cbc201350dbb68cb8cbdfe4176703efd642affd62739dbff212dd9c6ea8d68518cae450dfbea54294f203a29466f44347199f5'
